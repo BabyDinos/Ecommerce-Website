fc3e1c799ef6eed4e237887c3217e37a6f51f850
Marcus Lau <108362808+BabyDinos@users.noreply.github.com>
2023-11-09 21:41:58 -0500

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :logger, level: :info
 config :ecommercewebsite, EcommercewebsiteWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 4000],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
 
