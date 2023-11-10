8bb124ea0297a6b3eca91f628845368d7b35a001
Marcus Lau <108362808+BabyDinos@users.noreply.github.com>
2023-11-10 16:28:48 -0500

    pool size changed

--- config/prod.exs
+++ config/prod.exs
@@ -30,4 +30,4 @@ config :ecommercewebsite, Ecommercewebsite.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
-  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  pool_size: 4 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
